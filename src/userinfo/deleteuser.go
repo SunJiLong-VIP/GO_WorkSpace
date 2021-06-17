055f3c80da8dd09ac09f3eb052873ca518676e59
SunJiLong-VIP <71575379+SunJiLong-VIP@users.noreply.github.com>
2021-06-17 09:04:26 +0800

    Delete src directory

--- src/userinfo/deleteuser.go
+++
@@ -1,7 +0,0 @@
-package userinfo
-
-import "fmt"
-
-func Deleteuser() {
-	fmt.Println("删除用户成功")
-}
--- src/userinfo/selectuser.go
+++
@@ -1,8 +0,0 @@
-package userinfo
-
-import "fmt"
-
-func Selectuser() {
-	fmt.Println("查询用户成功")
-
-}
